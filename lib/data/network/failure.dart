83c3a12db280fa9124df103af2ba71f4dbf698d3
kooku0 <koomg9599@gmail.com>
2024-08-20 22:25:29 +0900

    lecture 96

--- lib/data/network/failure.dart
+++ lib/data/network/failure.dart
@@ -1,6 +1,16 @@
+import 'package:complete_advanced_flutter/data/network/error_handler.dart';
+
 class Failure {
   int code; // 200 or 400
   String message; // error or success
 
   Failure({required this.code, required this.message});
+}
+
+class DefaultFailure extends Failure {
+  DefaultFailure()
+      : super(
+          code: ResponseCode.DEFAULT,
+          message: ResponseMessage.DEFAULT,
+        );
 }
