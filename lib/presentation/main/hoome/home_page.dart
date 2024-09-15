1669bf22ef3633b4b40bfa6da9c674e037e943c6
kooku0 <koomg9599@gmail.com>
2024-09-15 14:16:23 +0900

    lecture: 156

--- lib/presentation/main/hoome/home_page.dart
+++ lib/presentation/main/hoome/home_page.dart
@@ -1,6 +1,8 @@
 import 'package:complete_advanced_flutter/app/di.dart';
+import 'package:complete_advanced_flutter/presentation/common/state_renderer/state_render_impl.dart';
 import 'package:complete_advanced_flutter/presentation/main/hoome/home_viewmodel.dart';
 import 'package:complete_advanced_flutter/presentation/resources/strings_manager.dart';
+import 'package:complete_advanced_flutter/presentation/resources/values_manager.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatefulWidget {
@@ -25,12 +27,62 @@ class _HomePageState extends State<HomePage> {
 
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text(
-        AppStrings.home,
-        style: TextStyle(color: Colors.black),
+    return Center(
+      child: SingleChildScrollView(
+        child: StreamBuilder<FlowState>(
+          stream: _viewModel.outputState,
+          builder: (context, snapshot) {
+            return snapshot.data?.getScreenWidget(
+                  context: context,
+                  contentScreenWidget: _getContentWidget(),
+                  retryActionFunction: _viewModel.start,
+                  resetFlowState: _viewModel.resetFlowState,
+                ) ??
+                Container();
+          },
+        ),
       ),
     );
+  }
+
+  Widget _getContentWidget() {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        _getBannersCarousel(),
+        _getSection(AppStrings.services),
+        _getServices(),
+        _getSection(AppStrings.stores),
+        _getStores(),
+      ],
+    );
+  }
+
+  Widget _getSection(String title) {
+    return Padding(
+      padding: const EdgeInsets.only(
+        top: AppPadding.p12,
+        left: AppPadding.p12,
+        right: AppPadding.p12,
+        bottom: AppPadding.p2,
+      ),
+      child: Text(
+        title,
+        style: Theme.of(context).textTheme.displaySmall,
+      ),
+    );
+  }
+
+  Widget _getBannersCarousel() {
+    return const Center();
+  }
+
+  Widget _getServices() {
+    return const Center();
+  }
+
+  Widget _getStores() {
+    return const Center();
   }
 
   @override
