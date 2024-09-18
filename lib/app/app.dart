c7b5ca63e5b3f71dfad45a4c46db1a6728b4265d
kooku0 <koomg9599@gmail.com>
2024-09-18 15:12:29 +0900

    lecture: 176

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -1,5 +1,8 @@
+import 'package:complete_advanced_flutter/app/app_prefs.dart';
+import 'package:complete_advanced_flutter/app/di.dart';
 import 'package:complete_advanced_flutter/presentation/resources/routes_manager.dart';
 import 'package:complete_advanced_flutter/presentation/resources/theme_manager.dart';
+import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 
 class MyApp extends StatefulWidget {
@@ -15,9 +18,23 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
+  final AppPreferences _appPreferences = instance<AppPreferences>();
+
+  @override
+  void didChangeDependencies() {
+    _appPreferences.getLocale().then((locale) {
+      context.setLocale(locale);
+    });
+
+    super.didChangeDependencies();
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      localizationsDelegates: context.localizationDelegates,
+      supportedLocales: context.supportedLocales,
+      locale: context.locale,
       onGenerateRoute: RouteGenerator.getRoute,
       initialRoute: Routes.splashRoute,
       theme: getApplicationTheme(),
