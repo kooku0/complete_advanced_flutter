10d66028530527f81948f4b92e725d9fb36d17f5
kooku0 <koomg9599@gmail.com>
2024-09-18 14:30:49 +0900

    lecture: 170

--- lib/presentation/main/settings_page.dart
+++ lib/presentation/main/settings_page.dart
@@ -1,4 +1,8 @@
+import 'package:complete_advanced_flutter/app/app_prefs.dart';
+import 'package:complete_advanced_flutter/app/di.dart';
+import 'package:complete_advanced_flutter/data/data_source/local_data_source.dart';
 import 'package:complete_advanced_flutter/presentation/resources/assets_manager.dart';
+import 'package:complete_advanced_flutter/presentation/resources/routes_manager.dart';
 import 'package:complete_advanced_flutter/presentation/resources/strings_manager.dart';
 import 'package:complete_advanced_flutter/presentation/resources/values_manager.dart';
 import 'package:flutter/material.dart';
@@ -12,43 +16,71 @@ class SettingsPage extends StatefulWidget {
 }
 
 class _SettingsPageState extends State<SettingsPage> {
+  final AppPreferences _appPreferences = instance<AppPreferences>();
+  final LocalDataSource _localDataSource = instance<LocalDataSource>();
+
   @override
   Widget build(BuildContext context) {
     return ListView(
       padding: const EdgeInsets.all(AppPadding.p8),
       children: [
         ListTile(
-            title: Text(
-              AppStrings.changeLanguage,
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-            leading: SvgPicture.asset(ImageAssets.changeLangIc),
-            trailing: SvgPicture.asset(ImageAssets.settingsRightArrowIc),
-            onTap: () {}),
+          title: Text(
+            AppStrings.changeLanguage,
+            style: Theme.of(context).textTheme.headlineMedium,
+          ),
+          leading: SvgPicture.asset(ImageAssets.changeLangIc),
+          trailing: SvgPicture.asset(ImageAssets.settingsRightArrowIc),
+          onTap: _changeLanguage,
+        ),
         ListTile(
-            title: Text(
-              AppStrings.contactUs,
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-            leading: SvgPicture.asset(ImageAssets.contactUsIc),
-            trailing: SvgPicture.asset(ImageAssets.settingsRightArrowIc),
-            onTap: () {}),
+          title: Text(
+            AppStrings.contactUs,
+            style: Theme.of(context).textTheme.headlineMedium,
+          ),
+          leading: SvgPicture.asset(ImageAssets.contactUsIc),
+          trailing: SvgPicture.asset(ImageAssets.settingsRightArrowIc),
+          onTap: _contactUs,
+        ),
         ListTile(
-            title: Text(
-              AppStrings.inviteFriends,
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-            leading: SvgPicture.asset(ImageAssets.inviteFriendsIc),
-            trailing: SvgPicture.asset(ImageAssets.settingsRightArrowIc),
-            onTap: () {}),
+          title: Text(
+            AppStrings.inviteFriends,
+            style: Theme.of(context).textTheme.headlineMedium,
+          ),
+          leading: SvgPicture.asset(ImageAssets.inviteFriendsIc),
+          trailing: SvgPicture.asset(ImageAssets.settingsRightArrowIc),
+          onTap: _inviteFriends,
+        ),
         ListTile(
-            title: Text(
-              AppStrings.logout,
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-            leading: SvgPicture.asset(ImageAssets.logoutIc),
-            onTap: () {}),
+          title: Text(
+            AppStrings.logout,
+            style: Theme.of(context).textTheme.headlineMedium,
+          ),
+          leading: SvgPicture.asset(ImageAssets.logoutIc),
+          onTap: _logout,
+        ),
       ],
+    );
+  }
+
+  void _changeLanguage() {
+    // i will apply localisation Later
+  }
+
+  void _contactUs() {
+    // its a task for you to open any web page with dummy content
+  }
+
+  void _inviteFriends() {
+    // its a task to share app name with friends
+  }
+
+  void _logout() {
+    _appPreferences.logout(); // clear login flag from app prefs
+    _localDataSource.clearCache();
+    Navigator.pushReplacementNamed(
+      context,
+      Routes.loginRoute,
     );
   }
 }
